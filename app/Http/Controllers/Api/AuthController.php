ada9c0199cc79da60132faf121799ff24143cdf1
dopd <dopd@kaopiz.com>
2021-08-12 11:18:12 +0700

    tach validate ra file rieng

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginAuthRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -13,24 +14,7 @@ class AuthController extends Controller
     {
         $this->middleware('auth:api')->except('login');
     }
-    public function login(Request $request){
-        $messages = [
-            'email.required' => 'Địa chỉ email không được để trống',
-            'email.email' => 'Địa chỉ email không hợp lệ',
-            'password.required' => 'Mật khẩu không được để trống',
-            'password.min' => "Mật khẩu phải có ít nhất 6 ký tự",
-            'password.max' => 'Mật khẩu không được quá 12 ký tự'
-        ];
-        $validator = Validator::make($request->only(['email','password']), [
-            'password' => 'required|min:6|max:12',
-            'email' => 'required|email',
-        ], $messages);
-        if($validator->fails()){
-            return response()->json([
-                'errors'=> $validator->errors(),
-                'inputData'=>$request->only(['email','password'])
-            ],422);
-        }
+    public function login(LoginAuthRequest $request){
         $credenttials = $request->only(['email','password']);
         if(!$token = JWTAuth::attempt($credenttials)){
             return response()->json(['success'=>false],401);
